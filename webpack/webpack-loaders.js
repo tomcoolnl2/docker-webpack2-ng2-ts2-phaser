76f5fd617f2de535e357a5afbc593c0e090c0c56
Tom Cool <tom.cool@indivirtual.com>
2017-01-20 12:46:06 +0100

    Refactored into ES6.

--- webpack/webpack-loaders.js
+++ webpack/webpack-loaders.js
@@ -1,91 +1,67 @@
 'use strict';
 
 
-var CssLoader = function () {
-    return {
-        test: /\.css$/,
-        loader: 'style!css'
-    };
-};
+const CssLoader = () => ({
+    test: /\.css$/,
+    loader: 'style!css'
+});
 
-var FontLoader = function () {
-    return {
-        test: /\.(woff|woff2)(\?.*$|$)/,
-        loader: 'url-loader?importLoaders=1&limit=10000&name=assets/app/[hash].[ext]'
-    };
-};
+const FontLoader = () => ({
+    test: /\.(woff|woff2)(\?.*$|$)/,
+    loader: 'url-loader?importLoaders=1&limit=10000&name=assets/app/[hash].[ext]'
+});
 
+const HtmlLoader = () => ({
+    test: /\.html$/,
+    loader: 'raw',
+    exclude: /node_modules/
+});
 
-var HtmlLoader = function () {
-    return {
-        test: /\.html$/,
-        loader: 'raw',
-        exclude: /node_modules/
-    };
-};
+const ImageLoader = () => ({
+    test: /\.(jpeg|jpg|png|gif|svg)(\?.*$|$)/,
+    loaders: [
+        'file?hash=sha512&digest=hex&name=[hash].[ext]',
+        'image-webpack?{bypassOnDebug:true, progressive: true, optimizationLevel:7,interlaced:false,svgo:{plugins: [{removeViewBox: false},{removeEmptyAttrs: false}]}'
+    ]
+});
 
+const JavascriptLoader = () => ({
+    test: /\.js$/,
+    loader: 'babel!jshint',
+    exclude: /node_modules/
+});
 
-var ImageLoader = function () {
-    return {
-        test: /\.(jpeg|jpg|png|gif|svg)(\?.*$|$)/,
-        loaders: [
-            'file?hash=sha512&digest=hex&name=[hash].[ext]',
-            'image-webpack?{bypassOnDebug:true, progressive: true, optimizationLevel:7,interlaced:false,svgo:{plugins: [{removeViewBox: false},{removeEmptyAttrs: false}]}'
-        ]
-    };
-};
+const JsonLoader = () => ({
+    test: /\.json$/,
+    loader: 'json',
+    exclude: /node_modules/
+});
 
-
-var JavascriptLoader = function () {
-    return {
-        test: /\.js$/,
-        loader: 'babel!jshint',
-        exclude: /node_modules/
-    };
-};
-
-var JsonLoader = function () {
-    return {
-        test: /\.json$/,
-        loader: 'json',
-        exclude: /node_modules/
-    };
-};
-
-var LessLoader = function () {
-    return {
-        test: /\.less$/,
-        loader: 'style!css!less'
-    };
-};
+const LessLoader = () => ({
+    test: /\.less$/,
+    loader: 'style!css!less'
+});
 
 
-var SassLoader = function () {
-    return {
-        test: /\.scss$/,
-        loader: 'style!css!sass'
-    };
-};
+const SassLoader = () => ({
+    test: /\.scss$/,
+    loader: 'style!css!sass'
+});
 
-var SvgLoader = function () {
-    return {
-        test: /\.svg$/,
-        loader: 'svg-inline',
-        exclude: /node_modules/
-    };
-};
+const SvgLoader = () => ({
+    test: /\.svg$/,
+    loader: 'svg-inline',
+    exclude: /node_modules/
+});
 
-var TsLintLoader = function () {
-    return {
-        test: /\.ts$/,
-        loader: 'tslint-loader'
-    };
-};
+const TsLintLoader = () => ({
+    test: /\.ts$/,
+    loader: 'tslint-loader'
+});
 
+const TypescriptLoader = params => {
 
-var TypescriptLoader = function (params) {
-
-    var routerLoader = 'angular2-router-loader';
+    const routerLoader = 'angular2-router-loader';
 
     params = params || {};
 
@@ -104,42 +80,35 @@ var TypescriptLoader = function (params) {
 };
 
 //** START Phaser loaders **//
-var PixiLoader = function () {
-    console.log('********** Pixi ********** ')
-    return { 
-        test: /pixi\.js$/, 
-        loader: 'expose-loader?PIXI'
-    };
-};
+const PixiLoader = () => ({ 
+    test: /pixi\.js$/, 
+    loader: 'expose-loader?PIXI'
+});
 
-var PhaserSplitLoader = function () {
-    return {
-        test: /phaser-split\.js$/, 
-        loader: 'expose-loader?Phaser'
-    };
-};
+const PhaserSplitLoader = () => ({
+    test: /phaser-split\.js$/, 
+    loader: 'expose-loader?Phaser'
+});
 
-var P2Loader = function () {
-    return {
-        test: /p2\.js$/, 
-        loader: 'expose-loader?p2'
-    };
-};
+const P2Loader = () => ({
+    test: /p2\.js$/, 
+    loader: 'expose-loader?p2'
+});
 //** END Phaser loaders **//
 
 module.exports = {
-    CssLoader: CssLoader,
-    FontLoader: FontLoader,
-    ImageLoader: ImageLoader,
-    HtmlLoader: HtmlLoader,
-    JsonLoader: JsonLoader,
-    LessLoader: LessLoader,
-    JavascriptLoader: JavascriptLoader,
-    SvgLoader: SvgLoader,
-    TsLintLoader: TsLintLoader,
-    TypescriptLoader: TypescriptLoader,
-    SassLoader: SassLoader,
-    PixiLoader: PixiLoader,
-    PhaserSplitLoader: PhaserSplitLoader,
-    P2Loader: P2Loader
+    CssLoader,
+    FontLoader,
+    ImageLoader,
+    HtmlLoader,
+    JsonLoader,
+    LessLoader,
+    JavascriptLoader,
+    SvgLoader,
+    TsLintLoader,
+    TypescriptLoader,
+    SassLoader,
+    PixiLoader,
+    PhaserSplitLoader,
+    P2Loader
 };
